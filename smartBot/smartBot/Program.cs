06645bc1c16cbfe27924b2d07a83382e3e749f99
Daniil Chernyshev <des1nteresado@outlook.com>
2019-03-26 17:41:14 +0300

    Add connection with bot. Add event for receive messages.

--- smartBot/smartBot/Program.cs
+++ smartBot/smartBot/Program.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot;
 
 namespace smartBot
 {
     class Program
     {
+        private static TelegramBotClient Bot;
         static void Main(string[] args)
         {
+            Bot = new TelegramBotClient("800246490:AAEHF0OEn3wva-7zfOZBHYpRXwWUu_NOrj0");
+
+            Bot.OnMessage += BotOnMessageReceived;
+
+            var me = Bot.GetMeAsync().Result;
+            Console.Out.WriteLine("Name = {0}", me.FirstName);
+
+            Bot.StartReceiving();
+            Console.Read();
+            Bot.StopReceiving();
+        }
+
+        private static void BotOnMessageReceived(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            var message = e.Message;
+            Console.Out.WriteLine("message = {0}", message.Text);
         }
     }
 }
