023c00375cff904f1ca1f5c209bf0e6aa32e4660
Daniil Chernyshev <des1nteresado@outlook.com>
2019-03-27 13:41:26 +0300

    Add support DialogFlow Small-Talk Agent

--- smartBot/smartBot/Program.cs
+++ smartBot/smartBot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ApiAiSDK;
 using Telegram.Bot;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
@@ -8,10 +9,16 @@ namespace smartBot
     class Program
     {
         private static TelegramBotClient Bot;
+        static ApiAi apiAi;
         private const string KEY = "800246490:AAEHF0OEn3wva-7zfOZBHYpRXwWUu_NOrj0";
+        private const string DIALOG_KEY = "17e30d465e0f4c478e6b7d5407f89d7a";
+
         static void Main(string[] args)
         {
-            Bot = new TelegramBotClient("kek");//remove before push
+            Bot = new TelegramBotClient(KEY);//remove before push
+            AIConfiguration configuration = new AIConfiguration(DIALOG_KEY, SupportedLanguage.Russian);
+            apiAi = new ApiAi(configuration);
+
             Bot.OnMessage += BotOnMessageReceived;
             Bot.OnCallbackQuery += BotOnCallBackQueryReceived;
 
@@ -23,9 +30,12 @@ namespace smartBot
             Bot.StopReceiving();
         }
 
-        private static void BotOnCallBackQueryReceived(object sender, Telegram.Bot.Args.CallbackQueryEventArgs e)
+        private static void BotOnCallBackQueryReceived(object sender, Telegram.Bot.Args.CallbackQueryEventArgs e)//пуши по пунктам меню
         {
-            throw new NotImplementedException();
+            var buttonText = e.CallbackQuery.Data;
+            var name = $"{e.CallbackQuery.From.FirstName} {e.CallbackQuery.From.LastName}";
+            Console.Out.WriteLine($"{name} нажал кнопку {buttonText}");
+            Bot.AnswerCallbackQueryAsync(e.CallbackQuery.Id, $"Вы нажали кнопку {buttonText}");
         }
 
         private static async void BotOnMessageReceived(object sender, Telegram.Bot.Args.MessageEventArgs e)
@@ -50,6 +60,20 @@ namespace smartBot
                     await Bot.SendTextMessageAsync(message.From.Id, text);
                     break;
                 case "/keyboard":
+                    var replyKeyboard = new ReplyKeyboardMarkup(new[]
+                    {
+                        new[]
+                        {
+                            new KeyboardButton("Привет.."),
+                            new KeyboardButton("Hello!")
+                        },
+                        new[]
+                        {
+                            new KeyboardButton("Контакт") {RequestContact = true},
+                            new KeyboardButton("Геолокация") {RequestLocation = true}
+                        }
+                    });
+                    await Bot.SendTextMessageAsync(message.Chat.Id, "Выбор невелик..", replyMarkup: replyKeyboard);
                     break;
                 case "/menu":
                     var menuKeyboard = new InlineKeyboardMarkup(new[]
@@ -68,6 +92,9 @@ namespace smartBot
                     await Bot.SendTextMessageAsync(message.From.Id, "Выберите пункт меню", replyMarkup: menuKeyboard);
                     break;
                 default:
+                    var response = apiAi.TextRequest(message.Text);
+                    string answer = response.Result.Fulfillment.Speech == "" ? "Прости, я тебя не понял.." : response.Result.Fulfillment.Speech; //ответ от dialogflow
+                    await Bot.SendTextMessageAsync(message.Chat.Id, answer);
                     break;
             }
         }
